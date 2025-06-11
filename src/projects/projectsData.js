d32bf156096bf4b236b5ca30640290d63424b55d
Jeppe Guldager <104680158+JDGuldager@users.noreply.github.com>
2025-06-11 12:42:27 +0200

    skills

---
+++ src/projects/projectsData.js
@@ -0,0 +1,47 @@
+import projectOne from "../assets/project-1.png";
+import projectTwo from "../assets/project-2.png";
+import projectThree from "../assets/project-3.png";
+
+const projects = {
+    1: {
+        title: "SoMe Platform",
+        image: projectOne,
+        desciption: (
+            <>
+            <p>
+                Lorem ipsum
+            </p>
+            </>
+        ),
+        github: "https://github.com/JDGuldager",
+        demo: "https://github.com/JDGuldager"
+    },
+     2: {
+        title: "Sumsum",
+        image: projectTwo,
+        desciption: (
+            <>
+            <p>
+                Lorem ipsum
+            </p>
+            </>
+        ),
+        github: "https://github.com/JDGuldager",
+        demo: "https://github.com/JDGuldager"
+    },
+     3: {
+        title: "Turd",
+        image: projectThree,
+        desciption: (
+            <>
+            <p>
+                Lorem ipsum
+            </p>
+            </>
+        ),
+        github: "https://github.com/JDGuldager",
+        demo: "https://github.com/JDGuldager"
+    },
+};
+
+export default projects;
---
+++ src/skills/skillsData.js
@@ -0,0 +1,66 @@
+const skills = {
+    1:[ {
+        title: "HTML",
+        level: 6, 
+    },
+    {
+        title: "CSS",
+        level: 5, 
+    },
+    {
+        title: "SASS",
+        level: 2, 
+    },
+    {
+        title: "JavaScript",
+        level: 5, 
+    },
+    {
+        title: "TypeScript",
+        level: 4, 
+    },
+    {
+        title: "Tailwind CSS",
+        level: 1, 
+    },
+    {
+        title: "React JS",
+        level: 3, 
+    },
+    {
+        title: "Figma",
+        level: 1, 
+    },
+    ],
+    2:[ {
+        title: "PostgreSQL",
+        level: 5, 
+    },
+    {
+        title: "Node.JS",
+        level: 2, 
+    },
+    {
+        title: "MongoDB",
+        level: 2, 
+    },
+    {
+        title: "Git",
+        level: 5, 
+    },
+    {
+        title: "SQL",
+        level: 3, 
+    },
+    {
+        title: "PHP",
+        level: 1, 
+    },
+    {
+        title: "ETC",
+        level: 3, 
+    },
+    ],
+};
+
+export default skills;
